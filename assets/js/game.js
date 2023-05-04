2d6287571d32f6e4e327fd208dd9d5a05ec45d5a
Mattice Heyzak <mattice.heyzak@gmail.com>
2023-05-04 14:07:15 +0200

    bug fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,13 @@
 class Game {
     constructor() {
+        this.fps = 30;
+    }
 
+    draw() {
+        drawRect('black', [0, 0, _$canvas.width, _$canvas.height]);
+    }
+
+    run() {
+        setInterval(() => this.draw(), 1000 / this.fps);
     }
 }
--- assets/js/initiater.js
+++ assets/js/initiater.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     initiateCanvas();
+    startGame();
 }
 
 function initiateCanvas() {
@@ -11,4 +12,9 @@ function initiateCanvas() {
     _ctx = _$canvas.getContext('2d');
     _$canvas.width = 1400;
     _$canvas.height = _$canvas.width * 16 / 30;
+}
+
+function startGame() {
+    const game = new Game();
+    game.run();
 }
