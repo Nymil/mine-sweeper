efb7de460cb2929a98e20fa01fc278a5428f098e
Mattice Heyzak <mattice.heyzak@gmail.com>
2023-05-04 13:46:37 +0200

    initiated canvas and added drawer

--- assets/js/initiater.js
+++ assets/js/initiater.js
@@ -11,5 +11,4 @@ function initiateCanvas() {
     _ctx = _$canvas.getContext('2d');
     _$canvas.width = 1400;
     _$canvas.height = _$canvas.width * 16 / 30;
-    drawRect('black', [0, 0, _$canvas.width, _$canvas.height]);
 }
